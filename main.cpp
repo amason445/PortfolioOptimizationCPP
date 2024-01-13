97f84e51f14b2d1516b4ee94b66648c4bc4f8ec0
Alex Mason <77219320+amason445@users.noreply.github.com>
2024-01-13 00:24:04 -0700

    Added comments

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@ int main() {
 	//instantiates rebalancing amount in account to 0 dollars
 	double rebalancing_amount;
 
-	//takes input for the current account balance in us dollars
+	//input for the current account balance in us dollars
 	std::cout << "Please enter the current account balance : " << std::endl;
 	std::cin >> rebalancing_amount;
 
 	//declares empty matrix and vectors
 	Eigen::MatrixXd A(1, 4);
 
+	//set dollar portfolio weights
 	A << 0.50, 0.25, 0.20, 0.05;
 
+	//transposed weight vector and multiplies by the rebalancing amount
 	Eigen::VectorXd x = rebalancing_amount * A.transpose();
 
 	std::cout << "Solution x:\n" << x << std::endl;
